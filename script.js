3fa446fcf4e6e50e55998ed141247818491a5c09
Amos Baya <amosbaya96@gmail.com>
2026-06-24 11:10:49 +0300

    Addede web3 forms implementation

--- script.js
+++ script.js
@@ -225,3 +225,40 @@ console.log('✅ Search functionality loaded successfully!');
         });
 
 
+        // contact form implimentation
+const form = document.getElementById('form');
+const submitBtn = form.querySelector('button[type="submit"]');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    formData.append("access_key", "e8052615-d6a7-4040-a3dd-934c91fbbc08");
+
+    const originalText = submitBtn.textContent;
+
+    submitBtn.textContent = "Sending...";
+    submitBtn.disabled = true;
+
+    try {
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            alert("Success! Your message has been sent.");
+            form.reset();
+        } else {
+            alert("Error: " + data.message);
+        }
+
+    } catch (error) {
+        alert("Something went wrong. Please try again.");
+    } finally {
+        submitBtn.textContent = originalText;
+        submitBtn.disabled = false;
+    }
+});
